0e72d86ae61b9a7f0c366b4aa3f038d940369eff
Erik Skinnari <erik@skinnari.se>
2021-02-21 22:51:13 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PhotoSorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
